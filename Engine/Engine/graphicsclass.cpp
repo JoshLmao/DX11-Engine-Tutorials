2f50f4787f1a864db53089d2cd220da15421e4e4
Josh Shepherd <josh_lmao@live.co.uk>
2017-01-06 19:46:34 +0000

    Completed Tut 3 on RasterTek. Using DirectX, window shows as grey

--- Engine/Engine/graphicsclass.cpp
+++ Engine/Engine/graphicsclass.cpp
@@ -2,7 +2,7 @@
 
 GraphicsClass::GraphicsClass()
 {
-
+	m_d3d = 0;
 }
 
 GraphicsClass::GraphicsClass(const GraphicsClass& other) 
@@ -17,20 +17,50 @@ GraphicsClass::~GraphicsClass()
 
 bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 {
+	bool result;
+
+	m_d3d = new D3DClass;
+	
+	if (!m_d3d)
+		return false;
+
+	result = m_d3d->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd, FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR);
+	if (!result)
+	{
+		MessageBox(hwnd, L"Could not initialize Direct3D", L"Error", MB_OK);
+		return false;
+	}
 	return true;
 }
 
 void GraphicsClass::Shutdown()
 {
+	if (m_d3d)
+	{
+		m_d3d->Shutdown();
+		delete m_d3d;
+		m_d3d = 0;
+	}
+
 	return;
 }
 
 bool GraphicsClass::Frame()
 {
+	bool result;
+
+	result = Render();
+	if (!result)
+		return false;
+	
 	return true;
 }
 
 bool GraphicsClass::Render()
 {
+	m_d3d->BeginScene(0.5f, 0.5f, 0.5f, 1.0f);
+
+	m_d3d->EndScene();
+
 	return true;
 }
